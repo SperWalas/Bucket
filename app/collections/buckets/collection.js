acea3cb9064f5d53ec6c9ef7fe668cef74b5c339
beunard <beunard@gmail.com>
2015-06-17 17:08:37 +0200

    [master] Parse and template buckets

--- app/collections/buckets/collection.js
+++ app/collections/buckets/collection.js
@@ -29,6 +29,60 @@ define([
         // On init model
         initialize : function() {},
 
+        parse: function(response) {
+            var contributors;
+            var accomplished;
+            var percent; 
+            var data;
+            var exist;
+            var index;
+
+            _.forEach(response, function(bucket) {
+                contributors = [];
+                accomplished = 0;
+                percent = 0;
+
+                _.forEach(bucket.tasks, function(task){
+                    
+                    if (task.accomplish) {
+                        accomplished++;
+                    }
+
+                    _.forEach(task.contributors, function(contributor) {
+
+                        exist = _.find(contributors, function(value) {
+                            return value.email === contributor.email;
+                        });
+
+                        if (exist === undefined) {
+                            data = {};
+                            data.name = contributor.name || 'User';
+                            data.email = contributor.email;
+                            data.tasks = [];
+                            data.tasks.push(task.id);
+                            contributors.push(data);
+                        } else {
+                            index = contributors.indexOf(exist);
+                            exist.tasks.push(task.id);
+                            contributors[index] = exist;
+                        }
+                    });
+                });
+
+                percent = (accomplished / bucket.tasks.length) * 100;
+
+                index = response.indexOf(bucket);
+
+                bucket.contributors = contributors;
+                bucket.percent = percent;
+                bucket.accomplished = accomplished;
+
+                response[index] = bucket;
+            });
+
+            return response;
+        }
+
 
     });
 
